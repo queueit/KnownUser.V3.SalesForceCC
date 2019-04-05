719bf5fb64230a607234208fb0b78cb6ebedc96a
mxb <mxbarnum@yahoo.com>
2019-04-05 11:57:23 -0400

    Updated to limit calls in SFRA

--- cartridges/int_queueit_sfra/cartridge/scripts/QueueIt.js
+++ cartridges/int_queueit_sfra/cartridge/scripts/QueueIt.js
@@ -9,7 +9,6 @@
 /* @modules */
 //var QueueIT = require('./queueit_knownuserv3_sdk.js')
 var objMgr = require('dw/object/CustomObjectMgr');
-var sitePrefs = require('dw/system/Site').getCurrent().getPreferences();
 var httpCtx = require('./httpContextProvider.js');
 
 const QueueIT = require("./queueit_knownuserv3_sdk.js"); 
@@ -19,18 +18,11 @@ const QueueIT = require("./queueit_knownuserv3_sdk.js");
  * Checks for custom site preferences, matches to request, redirects to queue it if appropriate. 
  *  
  */
-exports.Start = function() {
-    
+exports.Start = function(configs) {
+
 	var prov = httpCtx.httpContextProvider();
 	
 	var requestUrl = prov.getHttpRequest().getAbsoluteUri();
-	
-	// NOTE: probably want this to be inclusionary vs exclusionary: only do this logic if 
-	// page show / category show / add to cart Request Prefilter
-	if (requestUrl.toString().indexOf('__Analytics') >= 0)
-	{
-		return;
-	}
 	
 	var configurations = objMgr.getAllCustomObjects('queueit');
 	var integrationsConfigString = "";
@@ -40,29 +32,11 @@ exports.Start = function() {
 		integrationsConfigString = configurations.first().custom.queueitdata;
 	}
 	
-	
-	
 	if (integrationsConfigString != "")
 	{
-		var queueItEnabled = false; 
-		if ('queueItEnabled' in sitePrefs.getCustom()) {
-			queueItEnabled = sitePrefs.getCustom()["queueItEnabled"];
-		}
-		if (!queueItEnabled)
-		{
-			return;
-		}
-		
-		var customerId = '';
-		if ('queueItCustomerId' in sitePrefs.getCustom()) {
-			customerId = sitePrefs.getCustom()["queueItCustomerId"]; // Your Queue-it customer ID
-		}
-		
-		var secretKey = '';
-		if ('queueItSecretKey' in sitePrefs.getCustom()) {
-			secretKey = sitePrefs.getCustom()["queueItSecretKey"]; // Your 72 char secret key as specified in Go Queue-it self-service platform
-		}
-		
+		var configsArr = configs.split('|');
+		var customerId = configsArr[0]; // Your Queue-it customer ID
+		var secretKey = configsArr[1]; // Your 72 char secret key as specified in Go Queue-it self-service platform
 		
 		if (customerId != '' && secretKey != '')
 		{
